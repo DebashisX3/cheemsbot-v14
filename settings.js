b759a35e6d7c385e0b372a89301a9a275bfff0d5
DebashisX3 <herokumailx1@gmail.com>
2024-07-02 06:03:08 +0000

    owner number updated

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ global.walink = 'https://wa.me/qr/ZPZ7Z6WYJ752O1'
 global.thumbimage_url = 'https://assets.telegraphindia.com/abp/2022/Jun/1655905064_roddur.jpg'
 //new
 global.botname = '꧁﴿»✥«·-𝕯𝕯 𝕮𝖍𝖊𝖊𝖒𝖘-𝕭𝖔𝖙-·»✥«﴾꧂' //ur bot name
-global.ownernumber = '61488878802' //ur owner number
+global.ownernumber = '919339619072' //ur owner number
 global.ownername = '꧁﴿☬✞ 𝔇𝔢𝔟𝔞𝔰𝔥𝔦𝔰 𝔇𝔢𝔶 ✞☬﴾꧂™' //ur owner name
 global.websitex = "https://www.facebook.com/Debashi.Dey.X2/"
 global.wagc = "https://chat.whatsapp.com/C4NU6KefgoLL9W8vpbhbEY"
